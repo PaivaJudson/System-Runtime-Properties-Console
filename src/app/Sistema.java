f2647bd4efa214b1991466f72fa653d8b3b5b733
judsonpaiva <judson.paiva@hotmail.com>
2022-04-07 07:19:36 +0100

    Console em Java

--- src/app/Sistema.java
+++ src/app/Sistema.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.io.Console;
 import java.util.Properties;
+import java.util.Set;
 
 public class Sistema {
 
@@ -8,6 +10,23 @@ public class Sistema {
 		
 		Properties p = System.getProperties();
 		System.out.println(p.getProperty("java.version"));
+		
+		/*
+		Set<Object> pk = p.keySet();
+		for(Object key: pk) {
+			System.out.println(key + " "+ p.get(key));
+		}
+		*/
+		Console c = System.console();
+		
+		
+		System.out.println("Informe o user: ");
+		String user = c.readLine();
+		
+		System.out.println("Informe a senha: ");
+		char[] pass = c.readPassword();
+		
+		System.out.println("usuário =  "+user+ " pass = "+pass);
 
 	}
 
